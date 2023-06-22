be4e658af19c3a7063d2df88b8e69222dfdf9be5
porgokarim1 <60364286+porgokarim1@users.noreply.github.com>
2023-06-22 19:55:13 -0400

    integrating js in html instead

--- qrcode.js
+++
@@ -1,6 +0,0 @@
-// let first set a container to get the inputed data
-var qrcode = new QRCode(document.querySelector(".qrcode"));
-qrcode.makeCode("Why did you scanned me?")
-// Efficiently Generate QR code without extra rendering
-function generateQr(){  qrcode.makeCode(document.querySelector("input").value)
-}
